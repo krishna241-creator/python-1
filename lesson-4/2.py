a595695a2294138197dc7d5ea04e26fb92fb665c
krishna241-creator <krishnapreetam24@gmail.com>
2025-09-27 15:22:10 +0300

    first

---
+++ lesson-4/2.py
@@ -0,0 +1,10 @@
+print("number of marks obtained from subjects")
+x = int(input("english"))
+y = int(input("math"))
+z = int(input("science"))
+a = int(input("hindi"))
+
+sum = x+y+z+a
+
+percentage = (sum/400)*100
+print("the percantage obtained is ",percentage)
