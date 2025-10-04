b03a0db7270c5f903fa0120da098d4d894cec487
krishna241-creator <krishnapreetam24@gmail.com>
2025-10-04 15:12:01 +0300

    first

---
+++ lesson-6/1.py
@@ -0,0 +1,18 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("any one of the numbers have boolean value as false")    
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0:
+    print("either of the value is greater than zero")
+else:
+    print("no number is greater than zero")    
+if b > 0 or c > 0:
+    print("either of the value is greater than zero")
+else:    
+    print("no value is greater than zero")
