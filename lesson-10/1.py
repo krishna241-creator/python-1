585dbf56201d606f1293aa5a545857ae094ea8b3
krishna241-creator <krishnapreetam24@gmail.com>
2025-11-15 15:27:11 +0300

    first

---
+++ lesson-10/1.py
@@ -0,0 +1,9 @@
+char = input("enter your character: ")
+string = input("enter name: ")
+i = 0
+count = 0
+while i < len(string):
+    if string[i] == char:
+        count = count + 1
+    i = i + 1
+print("the occurrence of", char, "is equal to", count)        
