d55bb5dff77333ae233e1379448476cc0fbb31ca
krishna241-creator <krishnapreetam24@gmail.com>
2026-02-24 15:03:51 +0300

    first

---
+++ import pygame.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((720,810))
+game = False
+while not game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = True
+    
+    pygame.display.flip()
